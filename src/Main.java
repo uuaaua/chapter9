da932135b1b284beb7ed960277194096b8a6d170
uuaaua <241100@std.hi-joho.ac.jp>
2025-06-18 10:29:35 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        // 引数なし
-        Hero h = new Hero();//同じ名前
-        System.out.println(h.name + "のHPは" + h.hp + "です");
-        // 引数あり String
-        Hero h1 = new Hero("ミナト");//同じ名前
-        System.out.println(h1.name + "のHPは" + h1.hp + "です");
-        // 引数あり int
-        Hero h2 = new Hero(999);//同じ名前
-        System.out.println(h2.name + "のHPは" + h2.hp + "です");
-        // 引数2つ
-        Hero h3 = new Hero(999, "ミナト");//同じ名前
-        System.out.println(h3.name + "のHPは" + h3.hp + "です");
+        Hero h1 = new Hero("ミナト")
+        System.out.println(h1.name);
+        Hero h2 = new Hero();
+        System.out.primtln(h2.name);
     }
 }
